cc0bdac505732c1c15197bbe8e86c9cf4b16df59
Donald McKendrick <ddmckendrick@gmail.com>
2014-09-08 15:11:18 -0400

    Add a way to capture how often you want the todos to refresh

--- src/js/modules/todo_entry/add_todo_view.js
+++ src/js/modules/todo_entry/add_todo_view.js
@@ -5,11 +5,14 @@ module.exports = Backbone.View.extend({
   className: 'add-todo',
   template: require('./add_todo.handlebars'),
   events: {
-    'submit form': "addNew"
+    'submit form': "addNew",
+    'keyup input': "resizeInput"
   },
   addNew: function(e){
     e.preventDefault();
     var desc = this.$el.find('input').val();
+    var recurrance = this.$el.find('select').val();
+    console.log(recurrance);
     if (desc) {
       var newTodo = new TodoItem({description: desc});
       newTodo.save();
@@ -17,6 +20,7 @@ module.exports = Backbone.View.extend({
       this.$el.find('input').val("").focus();
     } else {
       this.$el.find('input').attr('placeholder', 'Try being a little more descriptive!');
+      this.$el.find('input').attr('width', '160px').attr('size', '9');
     }
   },
   initialize: function(){
@@ -28,5 +32,13 @@ module.exports = Backbone.View.extend({
     this.$el.html(this.template());
     return this;
   },
-
+  resizeInput: function() {
+    if (this.$el.find('input').val().length === 0){
+      this.$el.find('input').attr('size', 9);
+    } else {
+      console.log('resizing');
+      this.$el.find('input').attr('size', this.$el.find('input').val().replace(/\s+/g, '').length);
+      this.$el.find('input').removeAttr('width');
+    }
+  }
 })
